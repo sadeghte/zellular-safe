f329ceffea1b67882bfd27754c0057dcbd84b4b0
sadegh <sadeghte@gmail.com>
2025-02-11 21:52:14 +0330

    static render

--- app/agent/[agentId]/deposit/page.tsx
+++ app/agent/[agentId]/deposit/page.tsx
@@ -1,6 +1,8 @@
 import DepositPage from "@/app/components/DepositPage";
 import type { Metadata } from "next";
 
+export const dynamic = "force-static";
+
 export default function page() {
     return <DepositPage />
 }
--- app/agent/[agentId]/withdraw/page.tsx
+++ app/agent/[agentId]/withdraw/page.tsx
@@ -1,6 +1,8 @@
 import WithdrawPage from "@/app/components/WithdrawPage";
 import type { Metadata } from "next";
 
+export const dynamic = "force-static";
+
 export default function page() {
     return <WithdrawPage />
 }
